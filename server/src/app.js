4b870de305ccfcfcd553b33d47b29a408f5d6f16
nhatquanggit <nhatquang27042003@gmail.com>
2026-04-20 13:08:44 +0700

    fix: enable CORS for production

--- server/src/app.js
+++ server/src/app.js
@@ -8,7 +8,13 @@ import { errorHandler, notFoundHandler } from './middlewares/error.middleware.js
 const app = express();
 
 app.use(helmet());
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: false
+}));
+app.options('*', cors());
 app.use(express.json());
 app.use(morgan('dev'));
 
